50fc2504d1a7da9b085ebf5bb9b9ed76a086a35a
Clément Lherbier <clement.lherbier@reewayy.io>
2023-12-15 15:48:14 +0100

    add project

--- pages/openai.py
+++ pages/openai.py
@@ -2,12 +2,13 @@
 import streamlit as st
 from IPython.display import Image
 import requests
+import base64  # Importez la bibliothèque base64
 
 # Generate image from text
 import openai
 
 # Définissez votre clé API OpenAI
-openai.api_key = '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'
+openai.api_key = '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'
 
 def generate_and_display_image(prompt):
     # Generate image from text
@@ -21,16 +22,6 @@ def generate_and_display_image(prompt):
     # Afficher l'image
     st.image(image_url, caption="Generated Image", use_column_width=True)
 
-    # Télécharger l'image au clic d'un bouton
-    if st.button("Télécharger l'image"):
-        download_image(image_url, 'img.png')
-        st.success("Image téléchargée avec succès!")
-
-def download_image(url_img, img_name):
-    img = requests.get(url_img).content
-    with open(img_name, 'wb') as handler:
-        handler.write(img)
-
 def main():
     st.title("Génération d'image avec OpenAI")
 
@@ -43,6 +34,12 @@ def main():
             st.warning("Veuillez saisir un prompt.")
         else:
             generate_and_display_image(prompt)
+
+
+st.sidebar.title("LHERBIER Clément")
+st.sidebar.markdown("""
+    Le site <a href="https://noveldeglace.com/nouvelles/" style="color: white; text-decoration: none;">noveldeglace.com</a> est un site qui regroupe des traductions benevole de romans japonais en français.
+    """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
