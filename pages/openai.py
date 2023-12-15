6de1a74a05f1cc89d782348cf7d955aeeefdf40f
Clément Lherbier <clement.lherbier@reewayy.io>
2023-12-15 15:53:37 +0100

    add project

--- pages/openai.py
+++ pages/openai.py
@@ -8,7 +8,7 @@ import base64  # Importez la bibliothèque base64
 import openai
 
 # Définissez votre clé API OpenAI
-openai.api_key = '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'
+openai.api_key = 'YOUR_API_KEY'
 
 def generate_and_display_image(prompt):
     # Generate image from text
